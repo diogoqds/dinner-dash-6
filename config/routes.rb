a93d383b0ea500593da965a5b02ea209386c6fa3
Vinícius Gomes <vgdsouza@live.com>
2018-07-03 21:36:33 -0300

    Edit funcionando, falta deixar bonito

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch '/users/:id', to: 'users#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
